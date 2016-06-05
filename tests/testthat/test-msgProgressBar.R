c7a55292d532d50624de77a6f3b2ec39937016d5
Måns Magnusson <mons.magnusson@gmail.com>
2016-06-05 08:56:20 +0200

    Small updates and fixes

--- tests/testthat/test-msgProgressBar.R
+++ tests/testthat/test-msgProgressBar.R
@@ -17,5 +17,27 @@ test_that(desc="expect silent constructor",{
   expect_silent(msgProgressBar(100))
 })
 
+test_that(desc="expect iterator",{
+  test_bar <- function(iter){
+    bar <- msgProgressBar(iter)
+    for(j in seq_along(iter)){
+      bar$increment()
+      Sys.sleep(0.1/j)
+    }
+  }  
+  expect_message(test_bar(rep(c("a", "b"), 5)), regexp = "25%")
+  expect_message(test_bar(1:10), regexp = "|=+|")
+})
 
+test_that(desc="expect different increment step",{
+  test_bar <- function(iter){
+    bar <- msgProgressBar(iter)
+    for(j in 1:(iter/2)){
+      bar$increment(2)
+      Sys.sleep(0.1/j)
+    }
+  }  
+  expect_message(test_bar(10), regexp = "25%")
+  expect_message(test_bar(10), regexp = "|=+|")
+})
 
